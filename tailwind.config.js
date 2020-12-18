8a6e504775dfcfd36887479e153f0d80426a4cba
Kevin Liu <kevin@kliu.io>
2020-12-17 22:43:59 -0500

    Style post page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,93 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {
-            colors: {
-                orange: "#f5af28",
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        orange: "#f5af28",
+      },
+      fontFamily: {
+        title: ["Raleway", "sans-serif"],
+      },
+
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme("colors.gray.900"),
+            a: {
+              color: theme("colors.blue.700"),
+              "&:hover": {
+                color: theme("colors.blue.700"),
+                textDecoration: "none",
+              },
             },
-            fontFamily: {
-                title: ["Raleway", "sans-serif"],
+            "h2 a": {
+              color: theme("colors.gray.900"),
+              textDecoration: "none",
             },
+            ".tag a": {
+              textDecoration: "none",
+            },
+          },
         },
+
+        dark: {
+          css: {
+            color: "#7982a9",
+            a: {
+              color: "#9ECE6A",
+              "&:hover": {
+                color: "#9ECE6A",
+              },
+            },
+
+            "h2 a": {
+              color: "#A9B1D6",
+            },
+
+            h1: {
+              color: "#A9B1D6",
+            },
+            h2: {
+              color: "#A9B1D6",
+            },
+            h3: {
+              color: "#A9B1D6",
+            },
+            h4: {
+              color: "#A9B1D6",
+            },
+            h5: {
+              color: "#A9B1D6",
+            },
+            h6: {
+              color: "#A9B1D6",
+            },
+
+            strong: {
+              color: "#A9B1D6",
+            },
+
+            code: {
+              color: "#A9B1D6",
+            },
+
+            figcaption: {
+              color: theme("colors.gray.500"),
+            },
+
+            "::selection": {
+              backgroundColor: "#6f7bb635",
+            },
+          },
+        },
+      }),
     },
-    variants: {
-        extend: {},
+  },
+  variants: {
+    extend: {
+      typography: ["dark"],
     },
-    plugins: [],
+  },
+  plugins: [require("@tailwindcss/typography")],
 };
