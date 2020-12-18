d3c1c2fe93311809fe08b6bdf5cfae6fa1893624
Kevin Liu <kevin@kliu.io>
2020-12-17 20:35:48 -0500

    Commit home-page

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                orange: "#f5af28",
+            },
+            fontFamily: {
+                title: ["Raleway", "sans-serif"],
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
